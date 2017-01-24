b3fedfa847e390d5bd4524992ba3e8d80e7019a3
Jimmy Pocock <jimmypocock@yahoo.com>
2017-01-24 17:53:05 -0600

    got a home view working

---
+++ RoverpassDemo/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  RoverpassDemo
+//
+//  Created by Jimmy Pocock on 1/24/17.
+//  Copyright © 2017 Jimmy Pocock. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        let url: URL = API.campgroundSearchURL("austin texas")
+        print("\(url)")
+    }
+}
+
