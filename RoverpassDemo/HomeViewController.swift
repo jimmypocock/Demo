e36cd7274daaf532068cdfdc1137c7f2df1de04e
Jimmy Pocock <jimmypocock@yahoo.com>
2017-01-24 18:11:50 -0600

    created button to show search modal

--- RoverpassDemo/HomeViewController.swift
+++ RoverpassDemo/HomeViewController.swift
@@ -10,12 +10,9 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-
+    @IBAction func buttonClicked(_ sender: UIButton) {
         let url: URL = API.campgroundSearchURL("austin texas")
-        print("\(url)")
+        print("Button Clicked! URL: \(url)")
     }
 }
 
