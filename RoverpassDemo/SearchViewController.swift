9240b866aa915260751cb9c6a246670e4c58527f
Jimmy Pocock <jimmypocock@yahoo.com>
2017-01-24 19:13:29 -0600

    added search view controller

---
+++ RoverpassDemo/SearchViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SearchViewController.swift
+//  RoverpassDemo
+//
+//  Created by Jimmy Pocock on 1/24/17.
+//  Copyright © 2017 Jimmy Pocock. All rights reserved.
+//
+
+import UIKit
+
+// MARK: Look up: UISearchContainerViewController
+class SearchViewController: UIViewController {
+    
+}
